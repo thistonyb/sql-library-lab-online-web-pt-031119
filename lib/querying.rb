90c4a7d3edc83e75521f4733acdbf82651a39eb9
Tony Baker <this.tonybaker@gmail.com>
2019-05-18 05:30:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.title) FROM characters
   JOIN character_books ON character_books.character_id = characters.id
-  JOIN books ON book.id = character_books.book_id
+  JOIN books ON books.id = character_books.book_id
   GROUP BY character.name
   ORDER BY COUNT(books.title)"
 end
