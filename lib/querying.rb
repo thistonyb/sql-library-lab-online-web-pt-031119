09c8219e58ade9a17e7e78ce22f7431d2e455858
Tony Baker <this.tonybaker@gmail.com>
2019-05-18 04:38:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, series.name FROM authors
   JOIN series ON author.id = series.author_id
-  "
+  JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
