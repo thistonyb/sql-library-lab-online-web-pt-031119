a9938890ea9345a52e3fe7acf5f86b7451121657
Tony Baker <this.tonybaker@gmail.com>
2019-05-18 05:09:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,11 +24,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, FROM series
+  "SELECT series.title FROM series
   JOIN characters ON characters.series_id = series.id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  ORDER BY COUNT(characters.species) DESC LIMIT 1"
+  ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
