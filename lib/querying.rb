8fdeda4897d4ef37850ce870f21848e878986ade
Tony Baker <this.tonybaker@gmail.com>
2019-05-18 04:22:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT COUNT(species) FROM characters"
+  "SELECT COUNT(species) FROM characters
+  ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
